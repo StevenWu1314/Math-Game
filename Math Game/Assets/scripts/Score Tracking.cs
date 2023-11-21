7ec21e60f552782c9be325e7a45570ee2f62ce5f
StevenWu1314 <114301405+StevenWu1314@users.noreply.github.com>
2023-11-21 14:48:02 -0500

    added score tracking

---
+++ Math Game/Assets/scripts/Score Tracking.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class ScoreTracking : MonoBehaviour
+{
+    public TMPro.TMP_Text highscoreText;
+    public TMPro.TMP_Text scoreText;
+    [SerializeField] private int score = 0;
+    public int highScore = 0;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore");
+        highscoreText.text = "HighScore\n" + highScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score > highScore)
+        {
+            highScore = score;
+            highscoreText.text = "HighScore\n" + highScore.ToString();
+        }
+        scoreText.text = "score\n" + score.ToString();
+        
+        
+    }
+
+    public void IncreaseScore(int value)
+    {
+        score += value;
+    }
+}
