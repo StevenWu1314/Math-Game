3b75c082431b5258299cebc766ea0cb171fd9976
Walid Thomspon <117622993+AutoOverHeaven@users.noreply.github.com>
2023-11-21 13:18:09 -0500

    Added enemys and boss

---
+++ Math Game/Assets/Enemys Scripts/Enemy_Controller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Controller : MonoBehaviour
+{
+    public float enemyHealth;
+    public float enemySpeed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector3.down * enemySpeed;
+
+        if( enemyHealth <= 0) 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
