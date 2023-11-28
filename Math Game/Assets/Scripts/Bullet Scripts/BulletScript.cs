cd8fcdefceb2ca830a0321b163f8cda015df0a6b
AnimeNormie <150858034+AnimeNormie@users.noreply.github.com>
2023-11-28 16:00:10 -0500

    Debugged PlayerController & BulletScript

--- Math Game/Assets/Scripts/Bullet Scripts/BulletScript.cs
+++ Math Game/Assets/Scripts/Bullet Scripts/BulletScript.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
 	public float speed = 5f;
-	public float deactive_timer = 3f;
+	public float deactivate_timer = 3f;
 	
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("DeactivateGameObject", deactivate_Timer);
+        Invoke("DeactivateGameObject", deactivate_timer);
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
-	Attack();
     }
 	
 	void Move(){
@@ -26,12 +25,6 @@ public class BulletScript : MonoBehaviour
 		transform.position = temp;
 	}
 
-	void Attack() {
-		if(Input.GetKeyDown(KeyCode.K))
-		{
-			Instantiate(player_Bullet, attack_Point.position, Quaternion.identity);
-		}
-	}
 
 	void DeactivateGameObject()
 	{
